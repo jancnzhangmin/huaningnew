81ca1fe203dea2063c286a7c07333fb6c9a91421
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-06-16 16:42:28 +0800

    aa

--- app/assets/javascripts/my-app.js
+++ app/assets/javascripts/my-app.js
@@ -11,41 +11,57 @@ var mainView = myApp.addView('.view-main', {
 });
 
 // Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
+
+
+
+myApp.onPageInit('index',function(page){
+
+    mainView.router.refreshPage();
+    var mySwiper = myApp.swiper('.swiper-container', {
+        pagination:'.swiper-pagination',
+        spaceBetween: 100, // 100px between slides
+        loop:true,
+        autoplay: 6000,
+        speed: 600
     });
 });
 
+myApp.onPageInit('contentindex',function(page){
+
+    mainView.router.refreshPage();
+});
+
 // Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
+myApp.onPageInit('showcontent',function(page){
+
+    $(document).ready(function(){
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function () {
+            $(this).jPlayer("setMedia", {
+                title: "Big Buck Bunny",
+                m4v:$('#mp4url').val()
+
+
+            });
+        },
+        swfPath: "jplayer",
+        supplied: "ogv, m4v",
+        size: {
+            width: "100%",
+            height: "200px"
+        },
+        useStateClassSkin: true,
+        autoBlur: false,
+        smoothPlayBar: true,
+        keyEnabled: true,
+        remainingDuration: true,
+        toggleDuration: true
+    });
+
+    });
+});
+
+
 
 var mySwiper = myApp.swiper('.swiper-container', {
     pagination:'.swiper-pagination',
@@ -54,3 +70,9 @@ var mySwiper = myApp.swiper('.swiper-container', {
     autoplay: 6000,
     speed: 600
 });
+
+
+
+
+
+
