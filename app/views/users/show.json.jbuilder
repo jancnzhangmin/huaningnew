8a935ba70cc7c5043094378569a817408912859e
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-06-27 14:26:53 +0800

    create user model

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :password_digest, :tel, :vcode, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615004029) do
+ActiveRecord::Schema.define(version: 20160627061757) do
 
   create_table "banners", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 20160615004029) do
     t.string   "contentimedia_content_type", limit: 255
     t.integer  "contentimedia_file_size",    limit: 4
     t.datetime "contentimedia_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "password_digest", limit: 255
+    t.string   "tel",             limit: 255
+    t.string   "vcode",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
 end
